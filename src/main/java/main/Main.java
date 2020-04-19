40416cebe6c7c8f8299cfc558c327a764fb53322
FrancoCingolani <cingolani_franco@hotmail.com>
2020-04-19 20:00:53 -0300

    Validar seguridad de contraseña

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Main {
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException{
+		
+		Usuario usuario = new Usuario("franco", "123456", 'A');
+		System.out.println(usuario.validarContrasenia());
+
+		
+	}
+
+}
--- src/main/java/main/Usuario.java
+++ src/main/java/main/Usuario.java
@@ -1,10 +1,18 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Usuario {
 	
 	String nombre;
-	String contrasenia;
+	String contrasenia; //Buscar algún algoritmo para hashear la contraseña
 	char tipo;
+	int intentosMaximos = 3;
+	int cantidadIntentos = 0;
 	
 	public Usuario(String nombre, String contrasenia, char tipo) {
 		this.nombre = nombre;
@@ -32,6 +40,35 @@ public class Usuario {
 		this.tipo = tipo;
 	}
 	
+	public boolean iniciarSesion(String nombre, String contrasenia) {
+		if(nombre == this.nombre && contrasenia == this.contrasenia && this.cantidadIntentos < this.intentosMaximos) {
+			System.out.println("El usuario se ha logueado exitosamente.");
+			return true;
+		}else {
+			this.cantidadIntentos++;
+			return false;
+		}
+	}
+	
+	public String validarContrasenia() throws FileNotFoundException, IOException {
+		File file = new File("./files/topContraseniasInseguras.txt");  
+	    String cadena;
+	    FileReader f = new FileReader(file);
+	    BufferedReader b = new BufferedReader(f);
+	    while((cadena = b.readLine())!=null) {
+	    	if(cadena.equals(this.contrasenia)) {
+	    		return("Contraseña insegura");
+	    	}
+	    }
+	    b.close();
+	    return("Contraseña segura");    
+	}
+
+
+	
+	
 	
 
 }
+
+
