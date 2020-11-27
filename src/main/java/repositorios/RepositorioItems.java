25b0e0d114aa606a41f8b45643b065ff21e2f8ce
FrancoCingolani <cingolani_franco@hotmail.com>
2020-11-26 23:25:53 -0300

    Avances en OpEgreso

---
+++ src/main/java/repositorios/RepositorioItems.java
@@ -0,0 +1,41 @@
+package repositorios;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import main.CurrencyML;
+import main.DocumentoComercial;
+import main.Item;
+import main.MedioDePago;
+import repositorios.AbstractHibernateRepo;
+
+public class RepositorioItems extends AbstractHibernateRepo{
+	
+	private static RepositorioItems instance;
+
+	public static RepositorioItems getInstance() {
+		if (instance == null)
+			instance = new RepositorioItems();
+		return instance;
+	}
+	
+	
+	public List<Item> buscarItems() {
+		
+		Query query = this.getEntityManager().createQuery("FROM Item items");
+		
+		@SuppressWarnings("unchecked")
+		
+		List<Item> results = query.getResultList();
+		
+		System.out.println(results);
+		
+		return results;
+		
+	}
+
+}
--- src/main/java/server/OperacionEgresoController.java
+++ src/main/java/server/OperacionEgresoController.java
@@ -1,12 +1,17 @@
 package server;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import main.CurrencyML;
 import main.MedioDePago;
 import main.OperacionEgreso;
 import main.Proveedor;
+import main.TarjetaDeCredito;
+import repositorios.RepositorioDocumentosComerciales;
 import repositorios.RepositorioMedioDePago;
 import repositorios.RepositorioMonedas;
 import repositorios.RepositorioOpEgreso;
@@ -18,16 +23,29 @@ public class OperacionEgresoController {
 	
 	RepositorioMonedas repoMonedas = RepositorioMonedas.getInstance();
 	
-	public ModelAndView altaOpEgresoFront(Request req, Response res) {
+	public <T> ModelAndView altaOpEgresoFront(Request req, Response res) {
 		
 		RepositorioMonedas repoMonedas = RepositorioMonedas.getInstance();
 		RepositorioMedioDePago repoMedioDePago = RepositorioMedioDePago.getInstance();
+		RepositorioDocumentosComerciales repoDocComerciales = RepositorioDocumentosComerciales.getInstance();
+		RepositorioDocumentosComerciales repoItems = RepositorioDocumentosComerciales.getInstance();
 		
-		List<CurrencyML> currencyml = repoMonedas.buscarMonedas();	
-		List<MedioDePago> mediosDePago = repoMedioDePago.buscarMediosDePago();
+		List<T> currencyml = (List<T>) repoMonedas.buscarMonedas();	
+		//List<T> mediosDePago = (List<T>) repoMedioDePago.buscarMediosDePago();
+		List<T> documentosComerciales = (List<T>) repoDocComerciales.buscarDocumentosComerciales();	
+		List<T> documentosComeritemsciales = (List<T>) repoItems.buscarItems();	
 		
 		
-		return new ModelAndView(mediosDePago, "altaOpEgreso.hbs");
+		HashMap<String, List<T>> opEgresoOptions = new HashMap<String, List<T>>();
+
+	    // Agrego valores al Hashmap (nombreReferencia, List)
+		opEgresoOptions.put("monedas", currencyml);
+		//opEgresoOptions.put("mediosDePago", mediosDePago);
+		opEgresoOptions.put("documentosComerciales", documentosComerciales);
+		opEgresoOptions.put("items", items);
+		
+		
+		return new ModelAndView(opEgresoOptions, "altaOpEgreso.hbs");
 	}
 	
 	public ModelAndView altaOpEgreso(Request req, Response res) {
@@ -36,19 +54,22 @@ public class OperacionEgresoController {
 
 		String proveedor = req.queryParams("proveedor");
 		String fecha = req.queryParams("fecha");
-		String medioDePago = req.queryParams("medioDePago");
+		//String medioDePago = req.queryParams("medioDePago");
 		String items = req.queryParams("items");
 		String documentosComerciales = req.queryParams("documentosComerciales");
 		String organizacion = req.queryParams("organizacion");
 		
+		Date fechaMedioDePago = new Date();
+		TarjetaDeCredito medioDePago = new TarjetaDeCredito("785430001547", "246", fechaMedioDePago, "Franco", "DNI", "456123789");
 		
+		/*
 		opEgreso.setProveedor(proveedor);
 		opEgreso.setFecha(fecha);
 		opEgreso.setMedioDePago(medioDePago);
 		opEgreso.setItems(items);
 		opEgreso.setDocumentosComerciales(documentosComerciales);
 		opEgreso.setOrganizacion(organizacion);
-		
+		*/
 
 
 		RepositorioOpEgreso.getInstance().persist(opEgreso);
