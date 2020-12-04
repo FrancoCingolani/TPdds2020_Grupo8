b146053a9b218f012c888b350c30899137505c18
Ignacio Angel <igna.angel.94@gmail.com>
2020-12-04 12:45:14 -0300

    Suba JS y CSS varios + new gitignore
    No estaban trackeados

---
+++ target/classes/public/js/visualizadorStyle.js
@@ -0,0 +1,32 @@
+// Al cargar la pagina, oculto los 3 divs
+$( document).ready(function() {
+	$("#egresos").hide();
+	$("#ingresos").hide();
+	$("#proyectos").hide();
+});
+
+// Funcion para tratar los cambios en el dropdown selector
+$("#selector").change(function() {
+	switch ($("#selector").val()) {
+		case "e": // Muestra egresos
+			$("#egresos").show();
+			$("#ingresos").hide();
+			$("#proyectos").hide();
+			break;
+		case "i": // Muestra ingresos
+			$("#egresos").hide();
+			$("#ingresos").show();
+			$("#proyectos").hide();
+			break;
+		case "p": // Muestra proyectos
+			$("#egresos").hide();
+			$("#ingresos").hide();
+			$("#proyectos").show();
+			break;
+		case "default": // Muestra proyectos
+			$("#egresos").hide();
+			$("#ingresos").hide();
+			$("#proyectos").hide();
+			break;
+	}
+});
